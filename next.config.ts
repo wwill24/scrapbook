2f7e4fe9c3bdcffc239b2e0a7ddaaf90e4e1e137
William MacBook <wwilliamw24@gmail.com>
2025-11-12 22:21:20 -0800

    Minor change

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,12 @@
 import type { NextConfig } from "next";
 
+const isProd = process.env.NODE_ENV === 'production';
+
 const nextConfig: NextConfig = {
   outputFileTracingRoot: "/Users/williamwu/Desktop/scrapbook/love-scrapbook",
   output: 'export',
-  basePath: '/love-scrapbook',
-  assetPrefix: '/love-scrapbook/',
+  basePath: isProd ? '/scrapbook' : '',
+  assetPrefix: isProd ? '/scrapbook/' : '',
 };
 
 export default nextConfig;
